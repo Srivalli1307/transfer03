89ae18b70c1a8325c7a90f0c07c151bfe5f69930
91830 <91830@srivalli>
2022-11-11 12:55:19 +0530

    jenkins project

---
+++ src/main/java/Mavenjenkins/transfer03/App.java
@@ -0,0 +1,13 @@
+package Mavenjenkins.transfer03;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "welcome jenkins in maven project" );
+    }
+}
